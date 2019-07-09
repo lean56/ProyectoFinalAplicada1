519cff0d61e7b4fb32ac586c265abeae47a2c96c
leonardo emil <theflaco56@hotmail.com>
2019-07-09 16:29:09 -0400

    Creando Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuario { get; set; }
+        public Contexto() : base("Contr") { }
+    }
+}
