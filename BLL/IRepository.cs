33a21f23f9495ebf4f5f2856b9fa1b97e3cb637b
leonardo emil <theflaco56@hotmail.com>
2019-07-09 16:27:42 -0400

    Creando BLL

---
+++ BLL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<T> where T : class
+    {
+        bool Guardar(T entity);
+        bool Modificar(T entity);
+        T Buscar(int id);
+        List<T> GetList(Expression<Func<T, bool>> expression);
+    }
+}
