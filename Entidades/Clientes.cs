379d4ed5149736e69704025afda6f9f8af51939e
leonardo emil <theflaco56@hotmail.com>
2019-07-10 09:07:50 -0400

    Entidad Clientes

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+   public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Celular { get; set; }
+        public string Email { get; set; }
+        public DateTime Fecha { get; set; }
+
+    }
+}
